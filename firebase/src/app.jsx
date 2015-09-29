2a6fdff76ca94abf17de6b3a25ea2ded16565b5c
Hochul Shin <darkblue99@gmail.com>
2015-09-29 22:48:20 +0900

    Lecture 31 finished

--- firebase/src/app.jsx
+++ firebase/src/app.jsx
@@ -2,20 +2,40 @@ var React = require('react');
 var ReactFire = require('reactfire');
 var Firebase = require('firebase');
 var rootUrl = 'https://radiant-torch-5552.firebaseio.com/';
+var Header = require('./header');
+var List = require('./list');
 
 var App = React.createClass({
 	mixins: [ReactFire],
+	getInitialState: function() {
+		return {
+			items: {},
+			loaded: false
+		};
+	},
 	componentWillMount: function(){
-		this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');	//firebase API와 this.state.items를 bind --> API data가 update되면 아래 render function도 다시 호출
+		fb = new Firebase(rootUrl + 'items/');
+		this.bindAsObject(fb, 'items');	//firebase API와 this.state.items를 bind --> API data가 update되면 아래 render function도 다시 호출
+		fb.on('value',this.handleDataLoaded);
 	},
 	render: function() {
-		console.log(this.state);
-		return <h1 >
-			Hello, React!
-			< /h1>
+		return <div className="row panel panel-default">
+			<div className="col-md-8 col-md-offset-2">
+				<h2 className="text-center">
+					To-Do List
+				</h2>
+				<Header itemsStore={this.firebaseRefs.items}/>
+				<div className={"content " + (this.state.loaded ? 'loaded' : '')}>
+					<List items={this.state.items}	/>
+				</div>
+			</div>
+		</div> 
+	},
+	handleDataLoaded : function(){
+		this.setState({
+				loaded: true
+			});	
 	}
 });
-
-
 var element = React.createElement(App, {});
 React.render(element, document.querySelector('.container'));
---
+++ firebase/src/list.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+
+var list = React.createClass({
+	render: function() {
+		return (
+			<ul>
+				{this.renderList()}
+			</ul>
+		);
+	},
+	renderList : function() {
+		if (this.props.items && Object.keys(this.props.items).length === 0) {
+			return <h4>
+				Add a todo to get started.
+			</h4>
+		} else {
+			var children = [];
+			for(var key in this.props.items){
+				children.push(
+					<li>
+						{this.props.items[key].text}
+					</li>
+				)
+			}
+			return children;
+		} 
+	}
+});
+
+module.exports = list;
