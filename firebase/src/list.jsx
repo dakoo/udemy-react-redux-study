3c83ac3784f7f42bc479f2bbb689a2fe04788a7c
Hochul Shin <hochul.shin0@gmail.com>
2015-09-30 18:25:41 +0900

    Section 5 finished

--- firebase/src/list.jsx
+++ firebase/src/list.jsx
@@ -1,25 +1,30 @@
 var React = require('react');
+var ListItem = require('./list-item');
 
 var list = React.createClass({
 	render: function() {
 		return (
-			<ul>
+			<div>
 				{this.renderList()}
-			</ul>
+			</div>
 		);
 	},
 	renderList : function() {
-		if (this.props.items && Object.keys(this.props.items).length === 0) {
+		if (!this.props.items) {
 			return <h4>
 				Add a todo to get started.
 			</h4>
 		} else {
 			var children = [];
 			for(var key in this.props.items){
+				var item=this.props.items[key];
+				item.key = key;
 				children.push(
-					<li>
-						{this.props.items[key].text}
-					</li>
+					<ListItem 
+						item={item} 
+						key={key}
+					>
+					</ListItem>
 				)
 			}
 			return children;
